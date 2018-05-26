125419cb4ef11ae56156ea694a9b262edb4b6eb8
Kevin <bianxudong02@163.com>
2018-05-26 21:58:19 +0800

    第一个爬虫实验
    从github爬取自己的用户信息

---
+++ 爬虫实验.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+URL = 'https://api.github.com'
+
+def build_uri(endpoint):
+    return '/'.join([URL,endpoint])
+
+def better_output(json_str):
+    return json.dumps(json.loads(json_str),indent = 4)
+
+def request_method():
+    response = requests.get(build_uri('users/NewerCN'))
+
+    print(better_output(response.text))
+
+if __name__ == '__main__':
+    request_method()
+
+
